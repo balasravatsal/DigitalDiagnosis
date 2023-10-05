0de9d24fcc60cf036e5ee6be8fe3bf4f641d16df
Arpit Mathur <arpitmathur23@gmail.com>
2023-10-05 16:17:50 +0530

    commit

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 import numpy as np
 import joblib
-from joblib import load
+from joblib import load,dump
 
 app = Flask(__name__)
 CORS(app)
