71a2fd8850e5f76f822eb91cd4ce7f1b9caa4e00
Vatsal <vatsalbalasra24@gmail.com>
2023-11-03 00:22:56 +0530

    vatsal here

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,23 @@
-import './App.css'
-// import ResponsiveAppBar from "./navbar/ResponsiveAppBar.jsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import SharedLayout from "./navbar/SharedLayout.jsx";
-import Home from "./pages/Home/Home.jsx";
-import Diagnosis from "./pages/Diagnosis/Diagnosis.jsx";
-import Subscription from "./pages/Subscription/Subscription.jsx";
-import Profile from "./pages/UserDetetails/Profile.jsx";
-import Account from "./pages/UserDetetails/Account.jsx";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SharedLayout from './navbar/SharedLayout.jsx';
+import Home from './pages/Home/Home.jsx';
+import Diagnosis from './pages/Diagnosis/Diagnosis.jsx';
+import Subscription from './pages/Subscription/Subscription.jsx';
+import Profile from './pages/UserDetetails/Profile.jsx';
+import Account from './pages/UserDetetails/Account.jsx';
 import LoginSignupPage from './pages/Login/LoginSignupPage';
-
+import "./App.css"
 
 function App() {
 
     return (
         <>
-            {/*<ResponsiveAppBar/>*/}
-
-            <BrowserRouter>
+            <div className="app-container"> {/* Apply background image and styles to the entire app */}
+                <BrowserRouter>
                     <Routes>
-                        <Route path={'/'} element={
-                            <SharedLayout/>
-                        }>
-                            <Route index element={<LoginSignupPage/>} />
+                        <Route path={'/'} element={<SharedLayout />}>
+                            <Route index element={<LoginSignupPage />} />
                             <Route path={'/home'} element={<Home/>}/>
                             <Route path={'/diagnosis'} element={<Diagnosis/>} />
                             <Route path={'/subscription'} element={<Subscription/>} />
@@ -32,9 +28,9 @@ function App() {
                         </Route>
                     </Routes>
             </BrowserRouter>
-
+            </div>
         </>
-    )
+    );
 }
 
 export default App
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,24 @@
-// import React from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Button, Typography } from "@mui/material";
+import HealthImg from "./healthImg-removebg-preview.png";
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Grid container spacing={2} sx={{ display: "flex", justifyContent: "center", alignContent: "center", height: "90vh" }}>
+                <Grid item md={4} sx={{ display: "flex", justifyContent: "center", flexDirection: "column"}}>
+                   <div>
+                       <Typography variant={"h5"}>We understand the importance of your health and well-being. Our advanced disease prediction system is designed to provide you with insights into possible health conditions based on the symptoms you provide</Typography>
+                       <Link to="/diagnosis">
+                           <Button variant={"contained"} sx={{marginTop: "2rem"}}>Start Diagnosis</Button>
+                       </Link>
+                   </div>
+                </Grid>
+                <Grid item md={4} sx={{ display: "flex", justifyContent: "justify" }}>
+                    <img src={HealthImg} alt="Health" /> {/* Use the <img> element to display the image */}
+                </Grid>
+            </Grid>
         </div>
     );
 };
